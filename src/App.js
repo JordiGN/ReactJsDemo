3a1b0404bca06b7b35146de6d868be1f3f85b9de
Jordi Garcia <jordi@tokyoplay.com>
2020-07-29 11:38:51 -0500

    agregando comentarios y apuntes al codigo, ya funciona de manera mas adecuaa el primer ejercicio de la lista

--- src/App.js
+++ src/App.js
@@ -5,42 +5,55 @@ import List from './components/List';
 
 class App extends React.Component {
 
+//El estado son como las variables que puedes usar globalmente dentro de una misma clase
   state = {
     name: "",
     items: [],
   }
 
   componentWillMount(){
-    console.log("El componente se va a montar o algo asi");
+    //console.log("El componente se va a montar o algo asi");
   }
 
   componentDidMount(){
-    console.log("el componente ya se monto");
+    //console.log("el componente ya se monto");
   }
-
-  addItem = () => {
-    this.state.items.push(this.state.name);
-    this.setState({
+//Las arrowfunctions no necesitan tener llaves para encerrar el codigo siempre y cuando solamente retornen un elemento
+//En caso de que tuvieran mas de un elemento entonces si es necesario ponerlo entre llaves para que lo abarque correctamente
+//similar a los ifs
+  addItem = () =>
+    this.setState(state => ({
         name: "",
-        items: this.state.items,
-      })
-  };
-
-  // addItem = () => {
-  //   console.log("si esta dando el click");
-  // }
-
-  handleChange = (event) =>{
-    //console.log("event= "+ event.target.value);
+        items: [...state.items, state.name],//El spread operator (...) es un metodo que te permite obtener los elementos
+        //de una estructura de datos, ya sea un arreglo o un objeto, los elementos los regresa como elementos, no como 
+        //estructura
+      }))
+//Es importante que cuando se quiere modificar el State, se haga unicamente mediante la funcion setState, ya que esta es la 
+//que manda llamar al ciclo de vida y permite que la modificacion haya sido de forma adecuada, de lo contrario, estaremos
+//editanto las variables del estado pero la aplicacion no lo sabra.
+  handleChange = (event) =>
     this.setState({name : event.target.value});
-  };
 
   render(){
+    //las variables deben de estar dentro de funciones, ya sea funciones de la clase o dentro del metodo render
+    //exise una forma de obtener los valores de un objeto o arreglo mediante desconstruccion
+    //esto se hace en una variable, ya sea const o let, en la cual se asigna el valor
+    //en el caso de los objetos, se hace referencia al objeto entre corchetes
+    const {items} = this.state;
+    //Para renombrar la propiedad del objeto se hace una asignacion como si fuera llave/valor
+    //const {items: nuevonombre} = this.state;
+    //en el caso de los arreglos se hace referencia a la posicion del elemento que deseamos tener, ahi mismo se puede
+    //renombrar la variable
+    //const [fv,...aa] = array;
+
+    //ya que tenemos la variable descrontruida podemos usarla y pasarla como prop
+
+    //las funciones que se embeban en el HTML deben de ir sin parentesis, ya que cuando lo llevan la funcion se ejecuta
     return (
         <div className="App">
           <input type="text" value={this.state.name} onChange={this.handleChange} />
           <button type="submit" onClick={this.addItem} > Enviar </button >
-          <List itemsSent={this.state.items} />
+          <List itemsSent={items} />
         </div>
       );
     }  
--- src/components/List.js
+++ src/components/List.js
@@ -2,26 +2,18 @@ import React from 'react';
 
 class List extends React.Component{
 
-	state = {
-		name: "holis",
-		//list: [],
-	}
-
 	render(){
-
+		const {itemsSent} = this.props;
+		//siempre que se va a utilizar codigo JS se tiene que encerrar entre llaves para poderse renderear
 		return (
 			<div> 
 				<ul>
-					{this.props.itemsSent.map((item) => 
-											<li key={item.toString()} >{item}</li>
-											)}
+					{itemsSent.map((item) => 
+						<li key={item.toString()} >{item}</li>
+					)}
 				</ul>
 			</div>
-
 		);
-	
-
-
 	}
 
 }
